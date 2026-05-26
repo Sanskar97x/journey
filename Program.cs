0ebd8ebedfe24fc67f931845b8c691b7e3d54569
Sanskar97x <tamangsanskar3@gmail.com>
2026-05-26 15:19:59 +0545

    Enemies-Will-Kill-Themselves

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("My name is Sanskar.");
+Console.WriteLine("Day 1 of becoming a better .NET developer.");
+
+int currentYear = 2026;
+int graduationYear = 2029;
+
+Console.WriteLine($"Years to improve: {graduationYear - currentYear}");
